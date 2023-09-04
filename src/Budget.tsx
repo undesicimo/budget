3ca24223a26c482d6bcdc49a490128bf9a67f091
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-09-04 22:10:56 +0900

    fix button

--- src/Budget.tsx
+++ src/Budget.tsx
@@ -6,19 +6,21 @@ type P = {
 export default function Budget(props: P) {
 	const { inputValue, setInputValue } = props;
 	return (
-		<div className='flex flex-col align-middle'>
-			<h1 className='text-3xl mb-4'>予算設定してね</h1>
+		<div className='flex flex-col items-center'>
+			<h1 className='text-3xl mb-4 text-center'>予算設定してね</h1>
 			<input
-				className='h-12 w-full'
+				className='h-12 w-full text-center text-2xl'
 				type='number'
 				value={inputValue}
 				onChange={e => setInputValue(e.target.value)}
 			/>
-			<button
-				className='bg-indigo-700 text-2xl w-10 inline-block mt-5'
-				type='submit'>
-				決定
-			</button>
+			<div className='flex justify-center'>
+				<button
+					className='bg-indigo-700 text-2xl w-10 mt-5'
+					type='submit'>
+					決定
+				</button>
+			</div>
 		</div>
 	);
 }
